6c723191031d9452ca075f71ece4208d47527d4d
Vishnu Vijayan <vvijayan@purdue.edu>
2017-05-27 12:06:47 +0530

    Inject feathers service

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms'
 
 
 import { AppComponent }  from './app.component';
 import { LogsComponent } from './components/logs/logs.component'
 
 @NgModule({
-  imports:      [ BrowserModule, HttpModule ],
+  imports:      [ BrowserModule, HttpModule, FormsModule ],
   declarations: [ AppComponent, 
                   LogsComponent ],
   bootstrap:    [ AppComponent ]
